3ea61df22641eb41594af5ef6a1c4b80b71a75ed
tessbianchi <tesshbianchi@gmail.com>
2015-03-07 00:08:52 -0500

    dsgf

--- GameController.ts
+++ GameController.ts
@@ -11,8 +11,6 @@ module Game{
 		width;
 		height;
 		model;
-		gamma;
-		gameShallStart;
 		
 		constructor(gameloop,canvas,width,height,model){
 			this.gameloop = gameloop;
@@ -20,8 +18,6 @@ module Game{
 			this.width = width;
 			this.height = height;
 			this.model = model;
-			this.gameShallStart = false;
-			this.gamma = 0;
 		}
 		takeInput(){
 			 if(this.model instanceof GameOne){
@@ -34,10 +30,10 @@ module Game{
 		
 		gameOneTakeInput(){
 			if(this.gameCanStart()){
-				//console.log("start");
+				console.log("start");
 			 	this.startGameOne();
 			}else{
-				//console.log("nostart");
+				console.log("nostart");
 				this.model.notEnoughCategories();
 			}
 		}
@@ -48,45 +44,31 @@ module Game{
 			this.mobileClick = <any>this.mobileClick.bind(this);
 			this.canvas.addEventListener("click",this.mobileClick); 
 			var mostRecentState = 100;
-			var mostRecentTimeItWasBeingHeldSideways;
-			window.ondeviceorientation = function(event) {
-				console.log(self.gamma);
-				  self.gamma = Math.round(event.gamma);
-				  if(self.gamma>125 || self.gamma < 55){
+			var mostRecentTimeItWasBeingHeldSideways ;
+			 window.ondeviceorientation = function(event) {
+				  var gamma = Math.round(event.gamma);
+				  if(gamma>125 || gamma < 55){
 				  	mostRecentTimeItWasBeingHeldSideways  = new Date().getTime();
 				  }
-				  if (self.gamma > 125 && mostRecentState <= 125){
+				  if (gamma > 125 && mostRecentState <= 125){
 					self.model.setRecentPassOrFail(true);//they got the answer right
 					self.model.heldSideways = true;
 					
-				  }else if(self.gamma< 55 && mostRecentState >= 55){
+				  }else if(gamma< 55 && mostRecentState >= 55){
 					self.model.setRecentPassOrFail(false);//they got the answer wrong
 					self.model.heldSideways = true;
-					
-				  }else if (self.gamma >= 55 && self.gamma <= 125){
-				  	self.gameShallStart = true;
-				  	while((new Date().getTime()) - mostRecentTimeItWasBeingHeldSideways < 1000){
+				  }else if (gamma >= 55 && gamma <= 125){
+				  	if(!self.model.gameStarted){
+						self.model.countdown();
+						self.model.startGame(5);
+				  	}
+				  	while((new Date().getTime()) - mostRecentTimeItWasBeingHeldSideways < 2000){
 				  		//doNothing
 				  	}
 				  	self.model.heldSideways = false;
 				  }
-				mostRecentState  = self.gamma;
-			}
-			this.startDaGame();
-			
-		}
-		startDaGame(){
-			this.clearVariablesOne();
-			this.model.newItem = false;
-			var self = this;
-			console.log(!this.model.gameStarted + " " + this.gameShallStart);
-			var f = function(){self.startDaGame();};
-			var t = setTimeout(f,100);
-			if(!this.model.gameStarted && this.gameShallStart){
-				self.model.countdown();
-				self.model.startGame(5);
-				clearTimeout(t);
-			}
+				mostRecentState  = gamma;
+				}
 		}
 		gameCanStart():boolean{
 			for(var i = 0; i != this.model.chosenCategories.length; ++i){
@@ -164,18 +146,13 @@ module Game{
 			}
 		}
 		clickSelectTotalRounds(X,Y){
-			var leftArrowStartingX	= (260/375) * this.width;
-			var leftArrowStartingY =  (250/667) * this.height;
-			var leftArrowEndingY = (320/667)* this.height;
+			var leftArrowStartingX	= 260/375 * this.width;
+			var leftArrowStartingY =  250/667 * this.height;
+			var leftArrowEndingY = 290/667* this.height;
 			if(X > leftArrowStartingX && X  < this.width ){
 				if(Y > leftArrowStartingY && Y < leftArrowEndingY){
 					this.model.slideLeft(this.width);
-				}
-			}else if(X > 0 && X  < (50/375)*this.width ){
-				if(Y > leftArrowStartingY && Y < leftArrowEndingY){
-					this.model.slideRight(this.width);
-				}
-			}else{
+			}else
 				if(Y > 550/667 * this.height){
 					this.model.setTotalRounds();
 					this.model.selectedRoundNumber();
@@ -183,7 +160,6 @@ module Game{
 				}
 			}
 		}
-		
 		clickTwo(X,Y){
 			var buttonStartingX = 290/375 * this.width;
 			var buttonEndingX = 360/375 * this.width;
@@ -219,6 +195,7 @@ module Game{
 			this.model.gameOver = false;
 			this.model.gameStarted = false;
 			this.model.newItem = false;
+			this.model.heldSideways = false;
 			
 			while(this.model.playedWords.length > 0) {
    				 this.model.playedWords.pop();
